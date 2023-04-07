5989d7a49213e2b10ecf02386a1e2d34a3dffae9
Martin Walther <it@martin-walther.ch>
2023-04-07 16:43:36 +0200

    Remove file

--- data/almalinux/configpcli.ps1
+++
@@ -1,3 +0,0 @@
-Set-PowerCLIConfiguration -Scope AllUser -ParticipateInCEIP $true -Confirm:$false
-Set-PowerCLIConfiguration -Scope User -PythonPath '/usr/local/bin/python3.7' -Confirm:$false
-Get-EsxSoftwareDepot | Format-List *
